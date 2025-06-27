6a7b55020660b1a08206d4018cadc60d129fbaa7
InkGoomba <james112519@gmail.com>
2025-06-27 01:34:00 -0400

    Update 9

--- Main.py
+++ Main.py
@@ -21,19 +21,26 @@ class AccountFrame(ctk.CTkFrame):
         self.notes_string = ctk.StringVar(value=notes)
         self.email_string = ctk.StringVar(value=email)
 
-        self.site = ctk.CTkLabel(self, text=site, font=('Aptos', 20), width=675)
+        self.site = ctk.CTkLabel(self, text=site, font=('Aptos', 20))
         self.username = ctk.CTkEntry(self, textvariable=self.username_string, width=300)
         self.password = ctk.CTkEntry(self, textvariable=self.password_string, width=300)
         self.notes = ctk.CTkEntry(self, textvariable=self.notes_string, width=300)
         self.email = ctk.CTkEntry(self, textvariable=self.email_string, width=300)
+        self.username_label = ctk.CTkLabel(self, text="Username")
+        self.password_label = ctk.CTkLabel(self, text="Password")
+        self.notes_label = ctk.CTkLabel(self, text="Notes")
+        self.email_label = ctk.CTkLabel(self, text="Email")
         
-        self.site.pack(pady=10)
-        self.email.pack(pady=5)
-        self.username.pack(pady=5)
-        self.password.pack(pady=5)
-        self.notes.pack(pady=5)
-
-
+        self.site.grid(row=0, column=0, columnspan=2, padx=5, pady=10)
+        self.username_label.grid(row=1, column=0, padx=(175,10), pady=5)
+        self.password_label.grid(row=2, column=0, padx=(175,10), pady=5)
+        self.email_label.grid(row=3, column=0, padx=(175,10), pady=5)
+        self.notes_label.grid(row=4, column=0, padx=(175,10), pady=5)
+        self.username.grid(row=1, column=1, padx=5, pady=5)
+        self.password.grid(row=2, column=1, padx=5, pady=5)
+        self.email.grid(row=3, column=1, padx=5, pady=5)
+        self.notes.grid(row=4, column=1, padx=5, pady=5)
+        
 # Methods
 def start():
     global accounts_data
@@ -45,11 +52,14 @@ def start():
         with open(settings_data["keyFileLocation"], 'rb') as filekey:
             key_bytes = filekey.read()
         key_data_fernet = Fernet(key_bytes)
-    if os.path.isfile(settings_data["accountsFileLocation"]) and key_data_fernet!=None:
+    if os.path.isfile(settings_data["accountsFileLocation"]) and key_data_fernet != None:
         with open(settings_data["accountsFileLocation"], 'rb') as enc_file:
             encrypted_bytes = enc_file.read()
-        decrypted_bytes = key_data_fernet.decrypt(encrypted_bytes)
-        accounts_data = json.loads(decrypted_bytes.decode('utf-8'))
+        try:
+            decrypted_bytes = key_data_fernet.decrypt(encrypted_bytes)
+            accounts_data = json.loads(decrypted_bytes.decode('utf-8'))
+        except:
+            error_popup("Invalid Key. Must use the same key used to encrypt file")
 
 def open_app_info():    
     global app_info_window
@@ -58,7 +68,7 @@ def open_app_info():
         app_info_window.title("About Window")
         app_info_window.geometry("300x300")
         app_info_text = ctk.CTkLabel(app_info_window, text="Created by: James Meyers", font=('Aptos',15))
-        app_info_text2 = ctk.CTkLabel(app_info_window, text="Version: b.1.7", font=('Aptos',15))
+        app_info_text2 = ctk.CTkLabel(app_info_window, text="Version: b.1.9", font=('Aptos',15))
         app_info_text.pack(anchor="w")
         app_info_text2.pack(anchor="w")
     else:
@@ -102,6 +112,7 @@ def change_account_location(settings_data_text : ctk.StringVar):
         settings_data["accountsFileLocation"] = new_location
         with open("Settings.json", 'w') as file:
             json.dump(settings_data, file, indent=4)
+        start()
         settings_data_text.set(new_location)
 
 def change_key_location(settings_key_text : ctk.StringVar):
@@ -111,23 +122,26 @@ def change_key_location(settings_key_text : ctk.StringVar):
         settings_data["keyFileLocation"] = new_location
         with open("Settings.json", 'w') as file:
             json.dump(settings_data, file, indent=4)
+        start()
         settings_key_text.set(new_location)
 
 def display_accounts():
-    global settings_data
     global accounts_data
     for widget in main_frame.winfo_children():
         widget.destroy()
     account_frames = []
 
-    if os.path.isfile(settings_data["accountsFileLocation"]):
+    main_frame.grid_columnconfigure(0,weight=1)
+    main_frame.grid_columnconfigure(1, weight=1)
+
+    if accounts_data != None:
         accounts = accounts_data["accounts"]
         row_num = 0
         col_num = 0
         for account in accounts:
             account_frames.append(AccountFrame(master=main_frame, site=account["site"], username=account["username"], password=account["password"], email=account["email"], notes=account["notes"], fg_color="#808080"))
         for frame in account_frames:
-            frame.grid(row=row_num, column=col_num, padx=5, pady=5)
+            frame.grid(row=row_num, column=col_num, padx=5, pady=5, sticky="nsew")
             if col_num == 1:
                 row_num += 1
                 col_num = 0
@@ -139,15 +153,27 @@ def display_accounts():
 def export_accounts():
     global settings_data
     global accounts_data
-    key_bytes = None
-    if accounts_data == None:
-        error_popup("No data to save or export")
-    else:
-        with open(settings_data["keyFileLocation"], 'rb') as filekey:
-            key_bytes = filekey.read()
+    global key_data_fernet
 
-        fernet = Fernet(key_bytes)
-        
+    if accounts_data == None or key_data_fernet == None:
+        error_popup("Invalid key or account data to save or export")
+    else:
+        accounts_json = json.dumps(accounts_data, indent=4)
+        accounts_bytes = accounts_json.encode('utf-8')
+        encrypted_accounts = key_data_fernet.encrypt(accounts_bytes)
+
+        if os.path.isfile(settings_data["accountsFileLocation"]):
+            with open(settings_data["accountsFileLocation"],'wb') as encrypted_file:
+                encrypted_file.write(encrypted_accounts)
+        else:
+            encrypted_file_location = ctk.filedialog.asksaveasfilename(title="Save Encrypted Account File", defaultextension=".json", filetypes=[("JSON Files", "*.json")])
+            if os.path.isdir(os.path.dirname(encrypted_file_location)):
+                with open(encrypted_file_location, 'wb') as encrypted_file:
+                    encrypted_file.write(encrypted_accounts)
+                settings_data["accountsFileLocation"] = encrypted_file_location
+            else:
+                error_popup("Not a valid file location")
+
 def error_popup(error_message):
     global alert_info_window
     alert_info_window = ctk.CTkToplevel()
@@ -157,12 +183,50 @@ def error_popup(error_message):
     alert_info_text.pack()
 
 def add_account():
-    global settings_data
     global accounts_data
-    
+
     for widget in main_frame.winfo_children():
         widget.destroy()
-    
+
+    site_name = ctk.CTkEntry(main_frame)
+    username = ctk.CTkEntry(main_frame)
+    password = ctk.CTkEntry(main_frame)
+    email = ctk.CTkEntry(main_frame)
+    notes = ctk.CTkEntry(main_frame)
+    site_name_label = ctk.CTkLabel(main_frame, text="Site Name:")
+    username_label = ctk.CTkLabel(main_frame, text="Username:")
+    password_label = ctk.CTkLabel(main_frame, text="Password:")
+    email_label = ctk.CTkLabel(main_frame, text="Email:")
+    notes_label = ctk.CTkLabel(main_frame, text="Notes:")
+    submit_button = ctk.CTkButton(main_frame, text="Add Account", command=lambda: submit_account_data(site_name.get(), username.get(), password.get(), email.get(), notes.get()))
+    site_name_label.grid(row=0, column=0, padx=10, pady=10)
+    username_label.grid(row=1, column=0, padx=10, pady=10)
+    password_label.grid(row=2, column=0, padx=10, pady=10)
+    email_label.grid(row=3, column=0, padx=10, pady=10)
+    notes_label.grid(row=4, column=0, padx=10, pady=10)
+    site_name.grid(row=0, column=1, padx=10, pady=10)
+    username.grid(row=1, column=1, padx=10, pady=10)
+    password.grid(row=2, column=1, padx=10, pady=10)
+    email.grid(row=3, column=1, padx=10, pady=10)
+    notes.grid(row=4, column=1, padx=10, pady=10)
+    submit_button.grid(row=5, column=0, columnspan=2, padx=10, pady=10)
+
+def submit_account_data(site_name, username, password, email, notes):
+    global accounts_data
+    new_account = {
+        "site": site_name,
+        "username": username,
+        "password": password,
+        "email": email,
+        "notes": notes
+    }
+    if accounts_data == None:
+        accounts_data = {"accounts":[]}
+        accounts_data["accounts"].append(new_account)
+    elif "accounts" in accounts_data and isinstance(accounts_data["accounts"], list):
+        accounts_data["accounts"].append(new_account)
+    else:
+        accounts_data["accounts"] = [new_account]
 
 # Initialization
 app = ctk.CTk()
